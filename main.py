be72af1c482aeca1737696e20f59aab25248dafb
Zuan0x <zuan.0x@gmail.com>
2024-01-06 13:37:30 +0000

    Add cell class

--- main.py
+++ main.py
@@ -33,6 +33,9 @@ class Window (Tk):
         line = Line(start, end, color)
         line.draw(self.canvas)
     
+    def draw_cell(self, cell):
+        cell.draw(self.canvas)
+    
 
 class Point:
     def __init__(self, x, y):
@@ -49,14 +52,32 @@ class Line:
         print("Drawing line from", self.start.x, self.start.y, "to", self.end.x, self.end.y)
         canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=self.color, width=2)
 
+class Cell:
+    def __init__(self, x1, y1, x2, y2, has_left_wall=True, has_right_wall=True, has_top_wall=True, has_bottom_wall=True):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+
+    def draw(self, canvas):
+        if self.has_left_wall:
+            canvas.create_line(self.x1, self.y1, self.x1, self.y2, fill="#f00", width=2)
+        if self.has_right_wall:
+            canvas.create_line(self.x2, self.y1, self.x2, self.y2, fill="#f00", width=2)
+        if self.has_top_wall:
+            canvas.create_line(self.x1, self.y1, self.x2, self.y1, fill="#f00", width=2)
+        if self.has_bottom_wall:
+            canvas.create_line(self.x1, self.y2, self.x2, self.y2, fill="#f00", width=2)
+
 def main():
     win = Window(800, 600)
     win.draw_line(Point(0, 0), Point(800, 600), "#00f000")
     win.draw_line(Point(0, 600), Point(800, 0), "#00f")
-    win.draw_line(Point(0, 0), Point(800, 0))
-    win.draw_line(Point(0, 0), Point(0, 600))
-    win.draw_line(Point(800, 0), Point(800, 600))
-    win.draw_line(Point(0, 600), Point(800, 600))
+    win.draw_cell(Cell(0, 0, 100, 100))
     win.wait_for_close()
 
 
